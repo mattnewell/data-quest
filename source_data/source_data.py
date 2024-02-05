d23b6e79cc8fa866312bf308926d86a1536845d1
Matt Newell <matthew.newell@rearc.io>
2024-02-05 17:06:42 -0500

    Cleanup.

--- source_data/source_data.py
+++ source_data/source_data.py
@@ -6,14 +6,33 @@ import requests
 from bs4 import BeautifulSoup
 from smart_open import open
 
+# NOTE: These are super-hardcoded, and could be more flexible
+session = boto3.Session(profile_name='sandbox')
+bucket = 's3://newell-data-quest'
 headers = {
     'User-Agent': 'matthew.newell@rearc.io'
 }
+bls_endpoint = 'https://download.bls.gov'
+productivity_path = '/pub/time.series/pr/'
 
-endpoint = 'https://download.bls.gov'
-path = '/pub/time.series/pr/'
-session = boto3.Session(profile_name='sandbox')
-bucket = 's3://newell-data-quest'
+
+def source_bls_productivity():
+    url = list_urls(bls_endpoint, productivity_path)
+    for file in url:
+        chunk_to_s3(file, file.split("/")[-1])
+
+
+def source_datausa_nation_pop():
+    chunk_to_s3('https://datausa.io/api/data?Geography=01000US&measure=Population', 'datausa_nation_pop.json')
+
+
+def list_urls(endpoint, path):
+    response = requests.get(endpoint + path, headers=headers)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        raw_anchors = soup.find_all('a', href=True)
+        links = [endpoint + links['href'] for links in raw_anchors if links['href']]
+        return links[1:len(links)]
 
 
 def chunk_to_s3(source_url, s3_key):
@@ -30,28 +49,6 @@ def chunk_to_s3(source_url, s3_key):
                 if not chunk:
                     break
                 fout.write(chunk)
-
-
-def list_urls(endpoint, path):
-    response = requests.get(endpoint + path, headers=headers)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        raw_anchors = soup.find_all('a', href=True)
-        links = [endpoint + links['href'] for links in raw_anchors if links['href']]
-        return links[1:len(links)]
-
-
-def source_bls_productivity():
-    start = time.time()
-    url = list_urls(endpoint, path)
-    for file in url:
-        chunk_to_s3(file, file.split("/")[-1])
-    end = time.time()
-    print(end-start)
-
-
-def source_datausa_nation_pop():
-    chunk_to_s3('https://datausa.io/api/data?Geography=01000US&measure=Population', 'datausa_nation_pop.json')
 
 
 source_bls_productivity()
